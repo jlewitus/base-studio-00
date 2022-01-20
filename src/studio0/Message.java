180964e3d6db8c55091c712d5ad7525b66fef54b
carolinedeluca <carolinedeluca@carolines-mbp.dhcp.wustl.edu>
2022-01-20 12:05:59 -0600

    Hi!

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -5,5 +5,6 @@ public class Message {
 	public static void main(String args[]) {
 		System.out.println("Say hi to your group mates!");
 		System.out.println("Hey y'all :)");
+		System.out.println("Hi everyone!!");
 	}
 }
